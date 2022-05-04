e2d157af97fc09be6e5dbaccd6f86c87fcf6fc1b
simone andreozzi <simoneandreozzi8@gmail.com>
2022-05-04 17:04:04 +0200

    inizializzazione-header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<div class="header">
+    <img src="{{asset("images/dc-logo.png")}}" alt="">
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="{{route('comics-page')}}">COMICS</a>
+            </li>
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+            <li>
+                <a href="#">TV</a>
+            </li>
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+            <li>
+                <a href="#">FANS</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+            
+            
+        </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
+
+Route::get('/comics', function () {
+    return view('guest.comics');
+}) ->name('comics-page');
